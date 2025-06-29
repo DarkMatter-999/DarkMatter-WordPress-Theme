58b569f02594022848028e4804cec6275c7221d6
DarkMatter-999 <darkmatter999official@gmail.com>
2025-06-29 23:38:58 +0530

    feat: add home footer

--- patterns/hero.php
+++ patterns/hero.php
@@ -78,16 +78,7 @@
 		<div class="wp-block-buttons">
 		<!-- wp:button {"style":{"spacing":{"padding":{"left":"0","right":"0","top":"0","bottom":"0"}}}} -->
 		<div class="wp-block-button">
-			<a
-			class="wp-block-button__link wp-element-button"
-			style="
-				padding-top: 0;
-				padding-right: 0;
-				padding-bottom: 0;
-				padding-left: 0;
-			"
-			>View my Projects<span></span><span></span><span></span><span></span></a
-			>
+		<a class="wp-block-button__link wp-element-button" href="#projects" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0" rel="#projects">View my Projects<span></span><span></span><span></span><span></span></a>
 		</div>
 		<!-- /wp:button -->
 		</div>
